350459ce91359e88f5454ece24b5f58db274fadf
esasadam06 <48632551+esasadam06@users.noreply.github.com>
2021-06-15 14:29:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
 [ -d "/usr/share/jwtracker" ] && [ ! -L "/usr/share/jwtracker" ] && echo "Directory /usr/share/jwtracker already exists." || sudo mkdir /usr/share/jwtracker 
-
 sudo gem install http
-
 sudo cp modules/*.rb /usr/share/jwtracker/
-
 echo "Copied to /usr/bin/"
-
 sudo cp jwtracker /bin/
-
 sudo chmod +x /bin/jwtracker
